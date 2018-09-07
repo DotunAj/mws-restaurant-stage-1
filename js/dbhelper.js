2527e0f2d08261673cc45abf9237380faa8bab71
Ajala Ayodotun <dotunajala@gmail.com>
2018-09-07 23:59:34 +0100

    feat: Fetch restaurant data from development server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -11,27 +11,24 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
+  //TODO:Refactor code to use the fetch API
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+    fetch(DBHelper.DATABASE_URL)
+      .then(response => response.json())
+      .then(data => {
+        const restaurants = data;
         callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+      })
+      .catch(error => {
+        callback(error, null)
+      })
   }
 
   /**
